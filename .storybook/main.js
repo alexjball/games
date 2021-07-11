e4ae4151cbba7690ace8d78626f21e19c5d868d4
Alex Ball <me@alexjball.com>
2021-07-11 11:33:19 -0400

    Project configuration

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: [
+    "../components/**/*.stories.@(ts|tsx)",
+    "../stories/**/*.stories.@(ts|tsx)",
+    "../pages/**/*.stories.@(ts|tsx)",
+  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  roots: ["<rootDir>/game"],
+  testMatch: ["**/?(*.)+(test).+(ts|tsx)"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+};
