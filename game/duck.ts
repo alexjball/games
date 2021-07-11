bb525ae76403ab5e78c0e73670705e98a21767ae
Alex Ball <me@alexjball.com>
2021-07-11 11:32:07 -0400

    redux

---
+++ game/duck.ts
@@ -0,0 +1,62 @@
+import { useReducer, useState } from "react";
+import { GameState, otherStone, Reversi, Stone } from "./reversi";
+
+export type Action = Move | Pass | NewGame;
+
+type Move = {
+  type: "move";
+  r: number;
+  c: number;
+};
+
+type Pass = {
+  type: "pass";
+};
+
+type NewGame = {
+  type: "newGame";
+};
+
+class Duck {
+  startingStone: Stone = "black";
+  game = new Reversi();
+
+  public get state(): GameState {
+    return this.game.toJSON();
+  }
+
+  newGame() {
+    this.game.newGame(this.startingStone);
+    this.startingStone = otherStone(this.startingStone);
+  }
+
+  pass() {
+    this.game.pass();
+  }
+
+  move(r: number, c: number) {
+    if (this.game.isValidMove(r, c)) {
+      this.game.move(r, c);
+    }
+  }
+
+  reducer = (_: GameState, action: Action): GameState => {
+    switch (action.type) {
+      case "move":
+        this.move(action.r, action.c);
+        break;
+      case "pass":
+        this.pass();
+        break;
+      case "newGame":
+        this.newGame();
+        break;
+    }
+    return this.state;
+  };
+}
+
+export function useReversi() {
+  const [duck] = useState(() => new Duck());
+  return useReducer(duck.reducer, duck.state);
+}
