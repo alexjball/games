96f8e18b736db0c302c89d209ca4ee250a2a4490
Alex Ball <me@alexjball.com>
2021-07-11 16:53:10 -0400

    Consolidate styling
    Co-authored-by: sashamaryl <sashamaryl@gmail.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   stories: [
     "../components/**/*.stories.@(ts|tsx)",
-    "../stories/**/*.stories.@(ts|tsx)",
     "../pages/**/*.stories.@(ts|tsx)",
   ],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
