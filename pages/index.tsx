b30692f6c9ebfaec872034ba8f97cbdf75835a04
Alex Ball <me@alexjball.com>
2021-07-12 11:47:29 -0400

    Set title and favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,21 @@ import dynamic from "next/dynamic";
 import { createStore } from "../redux";
 import { Provider } from "react-redux";
 import TopBar from "../components/TopBar";
-
+import Head from "next/head";
 const GameContainer = dynamic(() => import("../components/GameContainer"), {
   ssr: false,
 });
 
 export default function Reversi() {
   return (
-    <Provider store={createStore()}>
-      <TopBar />
-      <GameContainer />
-    </Provider>
+    <>
+      <Head>
+        <title>Reversi</title>
+      </Head>
+      <Provider store={createStore()}>
+        <TopBar />
+        <GameContainer />
+      </Provider>
+    </>
   );
 }
