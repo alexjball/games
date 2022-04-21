6fdf7be5e685f2a9ec212d18ecd84b186a71df68
Alex Ball <me@alexjball.com>
2022-04-21 15:53:22 -0400

    More updates to clean up linter

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = {
   // TODO: re-enable once use-resize-observer is fixed
   // https://github.com/ZeeCoder/use-resize-observer/issues/90
   reactStrictMode: false,
+  eslint: {
+    dirs: ["components", "game", "pages", "redux", "styles"],
+    ignoreDuringBuilds: true,
+  },
 }
