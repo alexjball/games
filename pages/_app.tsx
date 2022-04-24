821bcd86831b9e0ea5a9c978c2af9d1e577ec737
Alex Ball <me@alexjball.com>
2022-04-24 10:53:28 -0400

    Add bootstrap, set up bootswatch theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app"
 import "../styles/game.css"
 import "../styles/globals.css"
+import "../styles/bootstrap.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
